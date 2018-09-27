8fdf9bb284a76241bd81311de9b06b8df3a7a71b
181192 <181192@stud.hvl.no>
2018-09-28 00:07:29 +0200

    Setting up project

---
+++ cryptography/02-CipherLab/src/main/java/no/kalli/Client.java
@@ -0,0 +1,63 @@
+package no.kalli;
+
+import picocli.CommandLine;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @author Kristoffer-Andre Kalliainen
+ */
+public class Client implements IParent {
+
+
+    public static void main(String[] args) {
+
+        CommandLine commandLine = new CommandLine(new Utility());
+        commandLine.parse(args);
+        if (commandLine.isUsageHelpRequested()) {
+            commandLine.usage(System.out);
+            return;
+        }
+
+        var client = new Client();
+        client.sendAndReceice();
+    }
+
+    private void sendAndReceice() {
+        Socket client;
+        ObjectOutputStream oos;
+        ObjectInputStream ois;
+
+        try {
+            client = new Socket("localhost", PORT);
+
+            System.out.println("Connected to Server on " + client.getInetAddress());
+
+            oos = new ObjectOutputStream(client.getOutputStream());
+            ois = new ObjectInputStream(client.getInputStream());
+
+            // send message to server
+//            oos.writeObject(encryptMessage(Utility.msg));
+            oos.flush();
+
+            // receive response from server
+            byte[] response = (byte[]) ois.readObject();
+//            response = decryptMessage(response);
+
+            System.out.println("Response from server: " + new String(response, StandardCharsets.UTF_8));
+
+            // close cliet socket
+            client.close();
+            ois.close();
+            oos.close();
+
+        } catch (ClassNotFoundException | IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ cryptography/02-CipherLab/src/main/java/no/kalli/IParent.java
@@ -0,0 +1,9 @@
+package no.kalli;
+
+/**
+ * @author Kristoffer-Andre Kalliainen
+ */
+public interface IParent {
+
+    int PORT = 9090;
+}
