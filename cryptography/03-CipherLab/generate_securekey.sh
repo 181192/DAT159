b4d477a1b742fe85c1b3afd04cc90aaf5470adae
181192 <181192@stud.hvl.no>
2018-10-07 21:47:31 +0200

    Refactored out keystore name to a variable, and check it passwordfile exists

--- cryptography/03-CipherLab/generate_securekey.sh
+++ cryptography/03-CipherLab/generate_securekey.sh
@@ -2,6 +2,7 @@
 
 DIRECTORY="src/main/resources"
 PASSWORD_FILE="$DIRECTORY/password.txt"
+KEYSTORE_NAME="des_keystore.jceks"
 
 #read -p "Enter your certificate password : " PASSWORD
 #echo "The password is: $PASSWORD"
@@ -15,10 +16,14 @@ if [ -f "$DIRECTORY"/keystore.jceks ]; then
     rm ${DIRECTORY}/keystore.jceks
 fi
 
+if [ -f "$PASSWORD_FILE" ]; then
+    rm ${PASSWORD_FILE}
+fi
+
 echo ${PASSWORD} >> ${PASSWORD_FILE}
 
-
-keytool -genseckey -alias securekey -keyalg des -keysize 56 -keypass ${PASSWORD} -storepass ${PASSWORD} -keystore ${DIRECTORY}/keystore.jceks -storetype jceks
+# Generate DES 56 bit secure key with alias "securekey"
+keytool -genseckey -alias securekey -keyalg des -keysize 56 -keypass ${PASSWORD} -storepass ${PASSWORD} -keystore ${DIRECTORY}/${KEYSTORE_NAME} -storetype jceks
 
 # Verify if the certificate was successfully added into the keystore.
-keytool -list -v -keystore ${DIRECTORY}/keystore.jceks -storetype jceks -storepass ${PASSWORD}
+keytool -list -v -keystore ${DIRECTORY}/${KEYSTORE_NAME} -storetype jceks -storepass ${PASSWORD}
