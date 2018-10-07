d1ecd49af9552e7342876ded90a058b450effcf6
181192 <181192@stud.hvl.no>
2018-10-07 21:46:17 +0200

    Changed to jceks keystore, and added check for files to delete

--- cryptography/03-CipherLab/generate_certificate.sh
+++ cryptography/03-CipherLab/generate_certificate.sh
@@ -2,6 +2,7 @@
 
 DIRECTORY="src/main/resources"
 PASSWORD_FILE="$DIRECTORY/password.txt"
+KEYSTORE_NAME="ssl_keystore.jceks"
 
 #read -p "Enter your certificate password : " PASSWORD
 #echo "The password is: $PASSWORD"
@@ -11,23 +12,35 @@ if [ ! -d "$DIRECTORY" ]; then
     mkdir ${DIRECTORY}
 fi
 
-if [ -d "$DIRECTORY" ]; then
-    rm -rf ${DIRECTORY}/*
+if [ -f "$DIRECTORY/$KEYSTORE_NAME" ]; then
+    rm ${DIRECTORY}/${KEYSTORE_NAME}
+fi
+
+if [ -f "$DIRECTORY/cacerts.jceks" ]; then
+    rm ${DIRECTORY}/cacerts.jceks
+fi
+
+if [ -f "$DIRECTORY/server.cer" ]; then
+    rm ${DIRECTORY}/server.cer
+fi
+
+if [ -f "$PASSWORD_FILE" ]; then
+    rm ${PASSWORD_FILE}
 fi
 
 echo ${PASSWORD} >> ${PASSWORD_FILE}
 
 # generate the certificates
-keytool -genkey -alias server-alias -keyalg RSA -keypass ${PASSWORD} -storepass ${PASSWORD} -keystore ${DIRECTORY}/keystore.jks
+keytool -genkey -alias server-alias -keyalg RSA -keypass ${PASSWORD} -storepass ${PASSWORD} -keystore ${DIRECTORY}/${KEYSTORE_NAME} -storetype jceks
 
 # Export the generated certificate to server.cer file.
-keytool -export -alias server-alias -storepass ${PASSWORD} -file ${DIRECTORY}/server.cer -keystore ${DIRECTORY}/keystore.jks
+keytool -export -alias server-alias -storepass ${PASSWORD} -file ${DIRECTORY}/server.cer -keystore ${DIRECTORY}/${KEYSTORE_NAME} -storetype jceks
 
 # Add the certificate to the trust store file
-keytool -import -v -trustcacerts -alias server-alias -file ${DIRECTORY}/server.cer -keystore ${DIRECTORY}/cacerts.jks -keypass ${PASSWORD} -storepass ${PASSWORD}
+keytool -import -v -trustcacerts -alias server-alias -file ${DIRECTORY}/server.cer -keystore ${DIRECTORY}/cacerts.jceks -keypass ${PASSWORD} -storepass ${PASSWORD} -storetype jceks
 
 # Verify if the certificate was successfully added into the keystore.
-keytool -list -v -keystore ${DIRECTORY}/keystore.jks
+keytool -list -v -keystore ${DIRECTORY}/${KEYSTORE_NAME} -storetype jceks -storepass ${PASSWORD}
 
 # Validate if the certificate was successfully added into the trust store.
-keytool -list -keystore ${DIRECTORY}/cacerts.jks
+keytool -list -keystore ${DIRECTORY}/cacerts.jceks -storetype jceks -storepass ${PASSWORD}
