02ee7645dd292e26dd87a952551bcc5c71c6e557
181192 <181192@stud.hvl.no>
2018-10-11 00:38:09 +0200

    Added Block

--- blockchain/01-SimpleBlockchain/src/main/java/no/kalli/Block.kt
+++ blockchain/01-SimpleBlockchain/src/main/java/no/kalli/Block.kt
@@ -1,24 +1,25 @@
 package no.kalli
 
+import java.io.UnsupportedEncodingException
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+import java.util.*
+import java.util.regex.Pattern
+import javax.xml.bind.DatatypeConverter
+
 /**
  * This class [Block] represents a block in the [Blockchain]. It takes the hash of the last block as
  * input to the constructor.
  */
-class Block(hashLastBlock: String) {
+class Block(hashLastBlock: String, // The data for this block.
+            val data: String) {
 
     var hash: String
     var nonce: Long = 0     // The nonce for this block.
-    var data: String = "" // The data for this block.
-    var prev: String = hashLastBlock // The hash of the previous block in the blockchain.
+    val prev: String = hashLastBlock // The hash of the previous block in the blockchain.
 
     init {
         this.hash = calculateHash()
-    }
-
-    /**
-     * Setting the data for this block
-     */
-    fun setData() {
     }
 
     /**
@@ -27,6 +28,10 @@ class Block(hashLastBlock: String) {
      * the hash must start with 5 zeroes.
      */
     fun mine(miningTarget: String) {
+        while (!Pattern.compile(miningTarget).matcher(hash).matches()) {
+            nonce++
+            hash = calculateHash()
+        }
     }
 
     /**
@@ -35,22 +40,25 @@ class Block(hashLastBlock: String) {
      * The method [isValidAsNextBlock] takes [hashLastBlock] the hash of the last block, and
      * the [miningTarget] mining target.
      */
-    fun isValidAsNextBlock(hashLastBlock: String, miningTarget: String): Boolean {
-        return false
-    }
+    fun isValidAsNextBlock(hashLastBlock: String, miningTarget: String): Boolean =
+            hashLastBlock  == prev && Pattern.compile(miningTarget).matcher(hash).matches()
 
     /**
      * The method [calculateHash] calculates the hash for this block based on the other instance variables.
      * The hash is returned as a HEX-String
      */
-    private fun calculateHash(): String {
-        return ""
-    }
+    fun calculateHash(): String = DatatypeConverter.printHexBinary(createSha256Hash(prev + nonce + data))
 
     /**
      * Amazing to-string method
      */
     override fun toString(): String {
-        return super.toString()
+        return "Block [data=$data\t nonce=$nonce\t hash=$hash\t prev=$prev]"
+    }
+
+    @Throws(NoSuchAlgorithmException::class, UnsupportedEncodingException::class)
+    private fun createSha256Hash(s: String): ByteArray {
+        val digest = MessageDigest.getInstance("SHA-256")
+        return digest.digest(s.toByteArray(charset("UTF-8")))
     }
 }
