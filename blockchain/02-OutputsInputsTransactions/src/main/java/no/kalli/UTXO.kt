04eb97e3e0d0e2718b568d670d7efa2926872602
181192 <181192@stud.hvl.no>
2018-10-22 22:04:39 +0200

    Implemented printUTXO, addOutputFrom and addAndRemoveOutputsFrom

--- blockchain/02-OutputsInputsTransactions/src/main/java/no/kalli/UTXO.kt
+++ blockchain/02-OutputsInputsTransactions/src/main/java/no/kalli/UTXO.kt
@@ -1,25 +1,37 @@
 package no.kalli
 
-import java.util.*
-
+/**
+ * This class represents a [UTXO]
+ */
 class UTXO {
 
-    //Why is this a Map and not a Set?
-    //  The values in this map are the UTXOs (unspent Outputs)
-    //  When removing UTXOs, we need to identify which to remove.
-    //  Since the Inputs are references to UTXOs, we can use those
-    //  as keys.
+    /**
+     * Why is this a Map and not a Set?
+     * The values in this map are the UTXOs (unspent Outputs)
+     * When removing UTXOs, we need to identify which to remove.
+     * Since the Inputs are references to UTXOs, we can use those
+     * as keys.
+     */
     private val map = HashMap<Input, Output>()
 
-    fun printUTXO() {
-        //TODO
-    }
+    /**
+     * [printUTXO] prints out the UTXO [map]
+     */
+    fun printUTXO() = map.forEach { t, u -> println("$t\t|\t$u") }
 
+    /**
+     * [addOutputFrom] add an [Output] from the [CoinbaseTx]
+     */
     fun addOutputFrom(ctx: CoinbaseTx) {
-        //TODO
+        map[Input("", -1)] = ctx.output
     }
 
+    /**
+     * [addAndRemoveOutputsFrom] add the [Output]'s in the [Transaction] to the UTXO [map],
+     * and removes the [Input]'s from the [map].
+     */
     fun addAndRemoveOutputsFrom(tx: Transaction) {
-        //TODO
+        tx.outputs.stream().map { map.put(Input(tx.txHash, tx.outputs.indexOf(it)), it) }
+        tx.inputs.stream().map { map.remove(it) }
     }
 }
