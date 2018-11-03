ef4eaff0c305d36bc88d74c9e294bf88734ea981
181192 <181192@stud.hvl.no>
2018-11-03 13:38:38 +0100

    Added serial print out and timeout

--- iot_cloud/01-IOTDeviceProgramming/access_control_device.ino
+++ iot_cloud/01-IOTDeviceProgramming/access_control_device.ino
@@ -22,6 +22,7 @@ void setup()
     pinMode(btnRight, INPUT);
     pinMode(pir, INPUT);
     startMillis = millis();
+    Serial.begin(9600);
 }
 
 void loop()
@@ -31,13 +32,12 @@ void loop()
     bool stateBtnRight = digitalRead(btnRight);
 
     currentMillis = millis();
-    // if (!statePir && (currentMillis - startMillis >= period))
-    // {
-    //     countBtnLeft, countBtnRight = 0;
-    //     digitalWrite(ledRed, HIGH);
-    //     digitalWrite(ledYellow, LOW);
-    //     digitalWrite(ledGreen, LOW);
-    // }
+    if (!statePir && (currentMillis - startMillis >= period))
+    {
+        Serial.println((String) "Timeout system for " + period + " ms locking");
+        startMillis = currentMillis;
+        lock(statePir);
+    }
 
     switch (state)
     {
@@ -63,6 +63,8 @@ void detectMotion(bool statePir)
 {
     if (statePir)
     {
+        Serial.println((String) "Detecting motion, state: " + state);
+        delay(200);
         digitalWrite(ledRed, LOW);
         state = 2;
     }
@@ -84,8 +86,10 @@ void waitForInput(bool stateBtnLeft, bool stateBtnRight)
         delay(300);
     }
 
-    if (countBtnLeft + countBtnRight == 2)
+    if (countBtnLeft + countBtnRight >= 2)
     {
+        Serial.println((String) "waitForInput state: " + state);
+        delay(200);
         state = 3;
     }
 }
@@ -94,15 +98,46 @@ void checkState()
 {
     if (countBtnLeft == 1 && countBtnRight == 1)
     {
+        Serial.println((String) "checkState state: " + state + " countBtnLeft: " + countBtnLeft + " countBtnRight: " + countBtnRight);
+        delay(200);
         digitalWrite(ledGreen, HIGH);
         state = 4;
     }
     else
     {
+        Serial.println((String) "checkState (blinkred) state: " + state + " countBtnLeft: " + countBtnLeft + " countBtnRight: " + countBtnRight);
+        delay(200);
         blinkRed();
-        countBtnLeft, countBtnRight = 0;
-        state = 2;
+        lock(1);
     }
+}
+
+void unlock(bool stateBtnLeft, bool stateBtnRight)
+{
+    Serial.println((String) "Unlock state: " + state);
+    delay(200);
+    digitalWrite(ledRed, LOW);
+    digitalWrite(ledYellow, LOW);
+    digitalWrite(ledGreen, HIGH);
+    delay(5000);
+    digitalWrite(ledRed, HIGH);
+    digitalWrite(ledGreen, LOW);
+
+    countBtnLeft = 0;
+    countBtnRight = 0;
+    Serial.println((String) "Reset count: " + stateBtnLeft + ", " + stateBtnRight);
+    delay(200);
+    state = 0;
+}
+
+void lock(bool statePir)
+{
+    Serial.println((String) "lock state: " + state + " statePir: " + statePir);
+    delay(200);
+    digitalWrite(ledRed, HIGH);
+    digitalWrite(ledGreen, LOW);
+    if (statePir)
+        state = 1;
 }
 
 void blinkRed()
@@ -114,24 +149,4 @@ void blinkRed()
         digitalWrite(ledRed, LOW);
         delay(200);
     }
-}
-
-void unlock(bool stateBtnLeft, bool stateBtnRight)
-{
-    digitalWrite(ledRed, LOW);
-    digitalWrite(ledYellow, LOW);
-    digitalWrite(ledGreen, HIGH);
-    delay(5000);
-    digitalWrite(ledRed, HIGH);
-
-    countBtnLeft, countBtnRight = 0;
-    state = 1;
-}
-
-void lock(bool statePir)
-{
-    digitalWrite(ledRed, HIGH);
-    digitalWrite(ledGreen, LOW);
-    if (statePir)
-        state = 1;
 }
