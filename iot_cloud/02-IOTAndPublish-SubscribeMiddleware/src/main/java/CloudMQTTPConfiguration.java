d62c0a9ebb8e2fa76d1e14b6c7d1d3fba382d63a
181192 <181192@stud.hvl.no>
2018-11-03 15:20:17 +0100

    Init project

---
+++ iot_cloud/02-IOTAndPublish-SubscribeMiddleware/src/main/java/CloudMQTTPConfiguration.java
@@ -0,0 +1,9 @@
+
+import io.dropwizard.Configuration;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.NotEmpty;
+
+public class CloudMQTTPConfiguration {
+
+
+}
