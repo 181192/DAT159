3ffab604d722e7a63057faa9ffbbb955049fda94
181192 <181192@stud.hvl.no>
2018-11-09 18:51:10 +0100

    Delete Temperature and Heat as config parameters

--- iot_cloud/02-IOTAndPublish-SubscribeMiddleware/src/main/java/no/kalli/cloudmqttp/Heat.kt
+++
@@ -1,3 +0,0 @@
-package no.kalli.cloudmqttp
-
-data class Heat(var topic: String = "", var qos: Int = 0)
--- iot_cloud/02-IOTAndPublish-SubscribeMiddleware/src/main/java/no/kalli/cloudmqttp/Temperature.kt
+++
@@ -1,3 +0,0 @@
-package no.kalli.cloudmqttp
-
-data class Temperature(var topic: String = "", var qos: Int = 0)
